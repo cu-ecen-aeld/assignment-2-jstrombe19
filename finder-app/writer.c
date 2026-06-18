9d51a292bd50927b4e36242d256f6700153e81dd
jstrombe19 <jaredstromberg@gmail.com>
2026-06-18 15:11:40 -0600

    minor syntax cleanup

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -1,37 +1,31 @@
 #include <syslog.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 
 /**
- * @brief 
- *  SYSLOG Entries
- * LOG_USER
- * LOG_DAEMON
- * LOG_LOCAL0 to LOG_LOCAL7
+ * @brief C implementation of writing string to a file given each at runtime
  * 
- * Priorities
- * LOG_ERR
- * LOG_WARNING
- * LOG_INFO
- * LOG_DEBUG
+ * @param argc 
+ * @param argv 
+ * @return int 
  */
-
 
 
 int main(int argc, char* argv[]) {
     openlog(NULL, 0 ,LOG_USER);
 
-    if (argc != 2) {
+    if (argc != 3) {
         syslog(LOG_ERR, "Invalid number of arguments: %d", argc);
         return 1;
     }
         
-    FILE *writefile = fopen(argv[0], "w");
+    FILE *writefile = fopen(argv[1], "w");
     if (writefile == NULL) {
-        syslog(LOG_ERR, "Failed to open file at provided path: %s", argv[0]);
+        syslog(LOG_ERR, "Failed to open file at provided path: %s", argv[1]);
         return 1;
     }
-    syslog(LOG_DEBUG, "Writing %s to %s", argv[1], argv[0]);
-    fwrite(argv[1], sizeof(argv[1]), 1, writefile);
+    syslog(LOG_DEBUG, "Writing %s to %s", argv[2], argv[1]);
+    fwrite(argv[2], strlen(argv[2]), 1, writefile);
 
 }
